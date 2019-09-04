749201080a198d433c6b09601e0620d5604254fe
Daiji NISHIMURA <bundai223@gmail.com>
2019-09-04 18:20:45 +0900

    update

--- chapter-2/2.2.rb
+++ chapter-2/2.2.rb
@@ -17,6 +17,6 @@ class Gear
   end
 end
 
-puts Gear.new(52, 11).ratio
+puts Gear.new(52, 11).ratio # 古いバージョンのコードが動かなくなってしまっているのが問題だよ、と言われている
 puts Gear.new(52, 11, 26, 1.5).gear_inches
 puts Gear.new(52, 11, 24, 1.25).gear_inches
