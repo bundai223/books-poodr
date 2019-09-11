14e65592c601eeeccaf1724477ab21d95f7297a8
Daiji NISHIMURA <bundai223@gmail.com>
2019-09-11 13:04:25 +0900

    p55

--- chapter-2/Gear.rb
+++ chapter-2/Gear.rb
@@ -1,11 +1,27 @@
 class Gear
-  attr_reader :chainring, :cog
-  def initialize(chainring, cog)
+  attr_reader :chainring, :cog, :wheel
+  def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
+    @wheel = Wheel.new(rim, tire)
   end
 
   def ratio
-    @chainring / @cog.to_f
+    chainring / cog.to_f
+  end
+
+  def gear_inches
+    ratio * wheel.diameter
+  end
+
+  # Wheelが再利用されないと判断した場合に、別クラスとして分離できる形を取りつつ、内包して決定を保留にすることもできる
+  Wheel = Struct.new(:rim, :tire) do
+    def diameter
+      rim + (tire * 2)
+    end
   end
 end
+
+puts Gear.new(52, 11, 26, 1.5).gear_inches
+puts Gear.new(52, 11, 24, 1.25).gear_inches
+
